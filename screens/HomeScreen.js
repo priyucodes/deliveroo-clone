5bed2a92fa70e69b3e06df13f3fd1cdbfdca4675
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-25 10:46:30 +0530

    Added icons package

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,34 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useLayoutEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{ uri: 'https://links.papareact.com/wru' }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">Current Location</Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
   );
 };
 export default HomeScreen;
