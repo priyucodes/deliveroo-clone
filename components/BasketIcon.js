40f93bc2a3231c9193d2abba48974361e4d89d43
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-26 00:19:20 +0530

    Added basket float on our restaurant screen

---
+++ components/BasketIcon.js
@@ -0,0 +1,28 @@
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useSelector } from 'react-redux';
+import Currency from 'react-currency-formatter';
+import { selectBasketItems, selectBasketTotal } from '../features/basketSlice';
+
+const BasketIcon = () => {
+  const items = useSelector(selectBasketItems);
+  const navigation = useNavigation();
+  // const basketTotal = useSelector(state => selectBasketTotal(state));
+  const basketTotal = useSelector(selectBasketTotal);
+  return (
+    <View className="absolute bottom-10 w-full z-50">
+      <TouchableOpacity className="mx-5 bg-[#00ccbb] p-4 rounded-lg flex-row items-center space-x-1">
+        <Text className="text-white font-extrabold text-lg bg-[#01a296] py-1 px-2">
+          {items.length}
+        </Text>
+        <Text className="flex-1 text-white font-extrabold text-lg text-center">
+          View Basket
+        </Text>
+        <Text className="text-lg text-white font-extrabold">
+          <Currency quantity={basketTotal} currency="INR" />
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+export default BasketIcon;
