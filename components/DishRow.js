ed2d92322d7fe300b57bd0c42fac00bc71fd3fdc
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-25 20:57:03 +0530

    Added restaurant screen and some ui

---
+++ components/DishRow.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const DishRow = ({ id, name, desceription, price, image }) => {
+  return (
+    <View>
+      <Text>DishRow</Text>
+    </View>
+  );
+};
+export default DishRow;
--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -2,6 +2,7 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { StarIcon } from 'react-native-heroicons/solid';
 import { MapPinIcon } from 'react-native-heroicons/outline';
 import { urlFor } from '../sanity';
+import { useNavigation } from '@react-navigation/native';
 const RestaurantCard = ({
   id,
   imgUrl,
@@ -14,8 +15,25 @@ const RestaurantCard = ({
   long,
   lat,
 }) => {
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity className="bg-white mr-3 shadow">
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('Restaurant', {
+          id,
+          imgUrl,
+          title,
+          rating,
+          genre,
+          address,
+          short_description,
+          dishes,
+          long,
+          lat,
+        });
+      }}
+      className="bg-white mr-3 shadow"
+    >
       <Image
         source={{ uri: urlFor(imgUrl).url() }}
         className="h-36 w-64"
