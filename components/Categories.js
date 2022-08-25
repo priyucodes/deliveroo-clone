2247a43d99ff9038602e9e6d26d7fc1fbf8609fc
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-25 12:45:14 +0530

    Added categories

--- components/Categories.js
+++ components/Categories.js
@@ -1,8 +1,23 @@
 import { View, Text, ScrollView } from 'react-native';
+import CategoryCard from './CategoryCard';
 const Categories = () => {
   return (
-    <ScrollView>
+    <ScrollView
+      contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    >
       {/* CategoryCard */}
+      <CategoryCard
+        imgUrl="https://img2.storyblok.com/filters:format(webp)/f/62776/512x256/e0988a5a22/curry-wide.jpg"
+        title="Butter Chicken Naan"
+      />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Sushi" />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="testing 3"
+      />
+
       <Text></Text>
     </ScrollView>
   );
--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,9 +1,12 @@
-import { View, Text } from 'react-native'
-const CategoryCard = () => {
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+const CategoryCard = ({ imgUrl, title }) => {
   return (
-    <View>
-      <Text>CategoryCard</Text>
-    </View>
-  )
-}
-export default CategoryCard
+    <TouchableOpacity className="relative mr-2">
+      <Image source={{ uri: imgUrl }} className="h-20 w-20 rounded" />
+      <Text className="absolute bottom-1 left-1 text-white font-bold">
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+export default CategoryCard;
