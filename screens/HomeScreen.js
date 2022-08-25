239d2a0974ea8826f1d3b409ce5a856f8d1d4d95
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-25 13:13:09 +0530

    Added restaurant cards

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,6 +9,7 @@ import {
   AdjustmentsVerticalIcon,
 } from 'react-native-heroicons/outline';
 import Categories from '../components/Categories';
+import FeaturedRow from '../components/FeaturedRow';
 const HomeScreen = () => {
   const navigation = useNavigation();
 
@@ -56,6 +57,21 @@ const HomeScreen = () => {
         {/* Categoriess */}
         <Categories />
         {/* Featured Rows */}
+        <FeaturedRow
+          id="123"
+          title="Featured"
+          description="Paid placements for our partners"
+        />
+        <FeaturedRow
+          id="1234"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts!"
+        />
+        <FeaturedRow
+          id="12345"
+          title="Offers near you!"
+          description="Why not support your local restaurant tonight!"
+        />
       </ScrollView>
     </SafeAreaView>
   );
