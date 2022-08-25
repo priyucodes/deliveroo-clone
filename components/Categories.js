6a3b5d2d3dbdadc463c085e667d4dcb8f1d79537
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-25 12:38:33 +0530

    fixed an bug in HomeScreen

---
+++ components/Categories.js
@@ -0,0 +1,10 @@
+import { View, Text, ScrollView } from 'react-native';
+const Categories = () => {
+  return (
+    <ScrollView>
+      {/* CategoryCard */}
+      <Text></Text>
+    </ScrollView>
+  );
+};
+export default Categories;
---
+++ components/CategoryCard.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+const CategoryCard = () => {
+  return (
+    <View>
+      <Text>CategoryCard</Text>
+    </View>
+  )
+}
+export default CategoryCard
