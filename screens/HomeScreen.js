d8a0096bd8186cb0dfcbd98d494e7fc6d37a7723
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-25 10:20:01 +0530

    Added RN navigator and configured tailwind

---
+++ screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-red-500">HomeScreen</Text>
+    </View>
+  );
+};
+export default HomeScreen;
