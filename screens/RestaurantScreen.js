68261dbd0689c40777f75655b8eec9b1f44a4e43
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-26 10:39:13 +0530

    Added basket screen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { useLayoutEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
 import {
   ArrowLeftIcon,
@@ -7,13 +7,16 @@ import {
   MapPinIcon,
   StarIcon,
 } from 'react-native-heroicons/solid';
+import { useDispatch } from 'react-redux';
 import { QuestionMarkCircleIcon } from 'react-native-heroicons/outline';
 import { urlFor } from '../sanity';
 import DishRow from '../components/DishRow';
 import BasketIcon from '../components/BasketIcon';
+import { setRestaurant } from '../features/restaurantSlice';
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   // const route = useRoute();
   const {
     params: {
@@ -29,6 +32,22 @@ const RestaurantScreen = () => {
       lat,
     },
   } = useRoute();
+  useEffect(() => {
+    dispatch(
+      setRestaurant({
+        id,
+        imgUrl,
+        title,
+        rating,
+        genre,
+        address,
+        short_description,
+        dishes,
+        long,
+        lat,
+      })
+    );
+  }, [dispatch]);
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
