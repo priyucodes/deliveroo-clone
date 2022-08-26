ad85c2b7eb78c4bc1277d0405dfe96b77da25ad8
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-26 11:49:42 +0530

    implemented delivery screen ui

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { XMarkIcon } from 'react-native-heroicons/solid';
 import * as Progress from 'react-native-progress';
 import { selectRestaurant } from '../features/restaurantSlice';
+import MapView, { Marker } from 'react-native-maps';
 const DeliveryScreen = () => {
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurant);
@@ -37,8 +38,37 @@ const DeliveryScreen = () => {
           </Text>
         </View>
       </SafeAreaView>
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.long,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        className="flex-1 mt-10 z-0"
+        mapType="mutedStandard"
+      >
+        <Marker
+          coordinate={{ latitude: restaurant.lat, longitude: restaurant.long }}
+          title={restaurant.title}
+          description={restaurant.short_description}
+          identifier="origin"
+          pinColor="#00ccbb"
+        />
+      </MapView>
 
-      
+      <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28">
+        <Image
+          source={{ uri: 'https://links.papareact.com/wru' }}
+          className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+        />
+        <View className="flex-1">
+          <Text className="text-lg">Priyanshu Bhandari</Text>
+          <Text className="text-gray-400">Your Rider</Text>
+        </View>
+
+        <Text className="text-[#00ccbb] text-lg mr-5">Call</Text>
+      </SafeAreaView>
     </View>
   );
 };
